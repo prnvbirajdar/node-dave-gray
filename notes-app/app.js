ca11354dadc44cc5952059804aaa739f735b9f41
prnvbirajdar <prnvbirajdar@gmail.com>
2022-07-01 17:18:57 -0400

    chalk package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,12 @@
 const validator = require('validator')
+const chalk = require('chalk')
+
 const getNotes = require('./notes')
 
 const msg = getNotes()
 
 console.log(msg);
+
+console.log(chalk.green.bold.bgWhite('success'));
 
 console.log(validator.isEmail('asda@gasc.com'));
