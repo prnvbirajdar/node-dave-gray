7e503a42ac05f8b1420c5c640ea4ec1d3b1051ed
prnvbirajdar <prnvbirajdar@gmail.com>
2022-07-03 22:35:14 -0400

    yargs commands crud

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,6 @@
 const validator = require('validator')
 const chalk = require('chalk')
+const yargs = require('yargs')
 
 const getNotes = require('./notes')
 
@@ -9,4 +10,36 @@ console.log(msg);
 
 console.log(chalk.green.bold.bgWhite('success'));
 
-console.log(validator.isEmail('asda@gasc.com'));
+console.log(process.argv);
+
+console.log(yargs.argv);
+
+// add command
+yargs.command({
+  command: 'add',
+  descrube: 'add a note',
+  handler: () => console.log('adding')
+})
+
+
+// add command
+yargs.command({
+  command: 'remove',
+  descrube: 'remove a note',
+  handler: () => console.log('remove')
+})
+
+// list command
+yargs.command({
+  command: 'list',
+  descrube: 'list notes',
+  handler: () => console.log('list notes')
+})
+
+
+// read command
+yargs.command({
+  command: 'read',
+  descrube: 'read a note',
+  handler: () => console.log('read')
+})
