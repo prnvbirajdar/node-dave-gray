0f79d77f6f7af91253430007059b8f67fe56ccbd
prnvbirajdar <prnvbirajdar@gmail.com>
2022-07-11 20:29:58 -0400

    notes crud

--- notes-app/app.js
+++ notes-app/app.js
@@ -3,36 +3,41 @@ const chalk = require('chalk')
 const yargs = require('yargs')
 
 const getNotes = require('./notes')
+const { argv } = require('yargs')
 
 const msg = getNotes()
-
-console.log(msg);
-
-console.log(chalk.green.bold.bgWhite('success'));
-
-console.log(process.argv);
-
-console.log(yargs.argv);
 
 // add command
 yargs.command({
   command: 'add',
-  descrube: 'add a note',
-  handler: () => console.log('adding')
+  describe: 'add a note',
+  builder: {
+    title: {
+      describe: 'Note title',
+      demandOption: true,
+      type: 'string'
+    },
+    body: {
+      describe: 'Note title body',
+      demandOption: true,
+      type: 'string'
+    }
+  },
+  handler: (argv) => console.log('adding', argv.title + " " + argv.body)
 })
 
 
 // add command
 yargs.command({
   command: 'remove',
-  descrube: 'remove a note',
+  describe: 'remove a note',
   handler: () => console.log('remove')
 })
 
 // list command
 yargs.command({
   command: 'list',
-  descrube: 'list notes',
+  describe: 'list notes',
   handler: () => console.log('list notes')
 })
 
@@ -40,6 +45,12 @@ yargs.command({
 // read command
 yargs.command({
   command: 'read',
-  descrube: 'read a note',
+  describe: 'read a note',
   handler: () => console.log('read')
 })
+
+yargs.parse()
+
+// console.log(process.argv);
+
+// console.log(yargs.argv);
